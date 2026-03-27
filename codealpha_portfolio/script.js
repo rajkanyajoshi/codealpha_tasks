ee607ff9f743c4e16cfa4e0a5035777d0cd6ae1e
Rajkanya Joshi <rajkanyajoshi13112006@gmail.com>
2026-03-27 15:50:34 +0530

    Add files via upload

---
+++ codealpha_portfolio/script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for internal links (future-ready)
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
