4abd0b091d74926516c213ea26059800a4967be8
Rajkanya Joshi <rajkanyajoshi13112006@gmail.com>
2026-03-27 15:26:59 +0530

    Delete CodeAlpha_Portfolio/script.js

--- CodeAlpha_Portfolio/script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for internal links (future-ready)
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
